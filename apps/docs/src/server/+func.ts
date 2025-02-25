c35fb2fa5eabcd482dcb8b57ddab11055c9792eb
rossrobino <rbrobino@gmail.com>
2025-02-25 15:12:53 -0500

    update deps

--- apps/docs/src/server/+func.ts
+++ apps/docs/src/server/+func.ts
@@ -2,44 +2,43 @@ import base from "@/content/base.md?raw";
 import color from "@/content/color-generator.md?raw";
 import overview from "@/content/overview.md?raw";
 import theme from "@/content/theme.md?raw";
-import { markdownProcessor } from "@/server/md";
-import { Injector } from "@robino/html";
+import { processor } from "@/server/md";
+import { Page } from "@robino/html";
+import { Router } from "@robino/router";
 import { html } from "client:page";
 import { html as colorHtml } from "client:page/color-generator";
 import type { Handler, Prerender } from "domco";
 
 export const prerender: Prerender = ["/", "/color-generator"];
 
-export const handler: Handler = (req) => {
-	const url = new URL(req.url);
+const router = new Router();
 
-	if (url.pathname === "/") {
-		const { html: overviewHtml } = markdownProcessor.process(overview);
-		const { html: themeHtml } = markdownProcessor.process(theme);
-		const { html: baseHtml } = markdownProcessor.process(base);
+router.get("/", async () => {
+	const { html: overviewHtml } = await processor.process(overview);
+	const { html: themeHtml } = await processor.process(theme);
+	const { html: baseHtml } = await processor.process(base);
 
-		const page = new Injector(html)
-			.comment("overview", overviewHtml)
-			.comment("theme", themeHtml)
-			.comment("base", baseHtml);
+	const page = new Page(html)
+		.inject("section-overview", overviewHtml)
+		.inject("section-theme", themeHtml)
+		.inject("section-base", baseHtml);
 
-		return new Response(page.toString(), {
-			headers: { "Content-Type": "text/html" },
-		});
-	} else if (url.pathname === "/color-generator") {
-		const { html: colorMdHtml } = markdownProcessor.process(color);
+	return page.toResponse();
+});
 
-		const page = new Injector(colorHtml).main(colorMdHtml, "prepend");
+router.get("/color-generator", async () => {
+	const { html: colorMdHtml } = await processor.process(color);
 
-		return new Response(page.toString(), {
-			headers: { "Content-Type": "text/html" },
-		});
-	} else if (url.pathname === "/oklch") {
-		url.pathname = "/color-generator";
-		url.search = "";
+	const page = new Page(colorHtml).inject("section-about", colorMdHtml);
 
-		return Response.redirect(url, 301);
-	}
+	return page.toResponse();
+});
 
-	return new Response("Not found", { status: 404 });
-};
+router.get("/oklch", ({ url }) => {
+	url.pathname = "/color-generator";
+	url.search = "";
+
+	return Response.redirect(url, 301);
+});
+
+export const handler: Handler = router.fetch;
--- apps/docs/src/server/md.ts
+++ apps/docs/src/server/md.ts
@@ -1,10 +1,10 @@
-import { MarkdownProcessor } from "@robino/md";
+import { Processor } from "@robino/md";
 import langBash from "shiki/langs/bash.mjs";
 import langCss from "shiki/langs/css.mjs";
 import langHtml from "shiki/langs/html.mjs";
 import langJs from "shiki/langs/js.mjs";
 
-export const markdownProcessor = new MarkdownProcessor({
+export const processor = new Processor({
 	highlighter: {
 		langs: [langBash, langCss, langJs, langHtml],
 	},
